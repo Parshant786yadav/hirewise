7aa2249e7d259e0d69dc9ee6943894ab046241e2
Parshant786yadav <Parshant786yadav@gmail.com>
2025-08-12 23:32:28 +0530

    Add

--- app.py
+++ app.py
@@ -201,6 +201,14 @@ def download_csv(csv_name):
 @app.route('/health')
 def health():
     return "OK", 200
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000)
